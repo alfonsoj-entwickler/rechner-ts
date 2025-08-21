a6f5e313e59278b61cf268d6ae6a7cf7848d2be7
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2025-08-21 20:00:12 +0200

    Refactor MenuItem component to use dispatch for adding items instead of a callback function

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,16 +1,18 @@
+import type { OrderActions } from "../reducers/order-reducer";
 import type { MenuItem } from "../types";
+import type { Dispatch } from "react";
 
 type MenuItemProps = {
   item: MenuItem;
-  addItem: (item: MenuItem) => void;
+  dispatch: Dispatch<OrderActions>;
 };
 
-export default function MenuItem({ item, addItem }: MenuItemProps) {
+export default function MenuItem({ item, dispatch }: MenuItemProps) {
   return (
     <button
       type="button"
       className="flex justify-between p-3 w-full border-2 border-teal-300 rounded-lg bg-transparent transition-colors hover:bg-teal-200"
-      onClick={() => addItem(item)}
+      onClick={() => dispatch({ type: "add-item", payload: { item } })}
     >
       <p>{item.name}</p>
       <p className="font-black">{item.price}€</p>
